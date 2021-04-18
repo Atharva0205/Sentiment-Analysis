ffa7665e0579bdf31deb18fc7f7898b997e52bef
Atharva0205 <72402981+Atharva0205@users.noreply.github.com>
2021-04-18 13:04:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 model = joblib.load('model')
 st.title('Sentiment Analyser')
 ip = st.text_input('Enter your remarks:')
-ip = ip.reshape(-1,1)
+ip = np.reshape(-1,1)
 op = model.predict([ip])
 if st.button('Predict'):
   st.title(op[0])
